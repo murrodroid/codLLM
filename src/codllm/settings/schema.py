3cd307b61617ad7985bc0a7c572f9d66f66739f9
Lucas Emil Bülow Mortensen <lucas37c@gmail.com>
2026-04-29 12:04:30 +0200

    changes to lr cosine

--- src/codllm/settings/schema.py
+++ src/codllm/settings/schema.py
@@ -92,7 +92,7 @@ class Config:
     save_strategy: SaveStrategy = "epoch"
     save_strategy_best_metric: SaveStrategyBestMetric = "macro_f1"
     model_task: ModelTask = "seq2seq"
-    lr_scheduler_type: LRSchedulerType = "linear"
+    lr_scheduler_type: LRSchedulerType = "cosine"
     verbose: bool = False
     output_dir: str = "./runs"
     seed: int = 42
